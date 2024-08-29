a7fd5b3d176a710e974a9eb498380629e8fd092b
01110011r <s.rasulov@tuit.uz>
2024-08-29 21:58:17 +0500

    fix:calculate_by_heron function optemized: variables type change from float to double

--- src/triangles_sorting.c
+++ src/triangles_sorting.c
@@ -18,9 +18,15 @@
 
 typedef struct triangle triangle;
 
-double calculate_by_heron(triangle* tr) {
-    float p = (tr->a+tr->b+tr->c)/2;
-    double S = sqrt(p*(p-tr->a)*(p-tr->b)*(p-tr->c));
+double calculate_by_heron(triangle *tr) {
+    double p = (tr->a + tr->b + tr->c) / 2.0; // Divide by 2.0 for accurate float division
+    double sub1 = p - tr->a;
+    double sub2 = p - tr->b;
+    double sub3 = p - tr->c;
+//    printf("p: %f, sub1: %f, sub2: %f, sub3: %f\n", p, sub1, sub2, sub3);
+
+    double S = sqrt(p * sub1 * sub2 * sub3);
+//    printf("S: %f\n", S);
     return S;
 }
 
@@ -30,9 +36,11 @@ void sort_by_area(triangle* tr, int n) {
     */
 
     for(int i=0; i<n-1; i++) {
-
+        printf("i: %d\n", i);
         for(int j=i+1; j<n; j++) {
+            printf("j: %d\n", j);
             if (calculate_by_heron(&tr[i])>calculate_by_heron(&tr[j])) {
+                printf("swap\n");
                 triangle swap=tr[i];
                 tr[i]=tr[j];
                 tr[j]=swap;
