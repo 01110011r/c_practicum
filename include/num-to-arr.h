2f961da9b9782c7039609b5a64966756a7157591
01110011r <s.rasulov@tuit.uz>
2024-11-20 23:47:06 +0500

    Codewars 'convert number to reversed array of digits' problem solved

---
+++ include/num-to-arr.h
@@ -0,0 +1,9 @@
+#ifndef NUM_TO_ARR_H
+#define NUM_TO_ARR_H
+
+#include <cstdint>
+#include <cstddef>
+
+void digitize(uint64_t n, uint8_t digits[], size_t *length_out);
+
+#endif // NUM_TO_ARR_H
---
+++ src/num-to-arr.c
@@ -0,0 +1,14 @@
+//Codewars 'convert number to reversed array of digits' problem solved.
+
+#include <stddef.h>
+#include <inttypes.h>
+
+void digitize (uint64_t n, uint8_t digits[], size_t *length_out)
+{
+  size_t length = 0;
+  do {
+    digits[length ++] = n%10;
+    n/=10;    
+  } while (n>0);
+  *length_out = length;
+}
